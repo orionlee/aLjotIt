53e3f2e9026d5e4f820d07cd0a553946e41233c5
orionlee <orionlee@yahoo.com>
2018-03-19 17:32:27 -0700

    Older (pre-Lollipop) device compatibility regression fix: re-add action bar header to SettingsActivity (which has gone missing due to the compatibility fix)

--- app/src/main/java/net/oldev/alsscratchpad/ThemeSwitcher.java
+++ app/src/main/java/net/oldev/alsscratchpad/ThemeSwitcher.java
@@ -6,6 +6,8 @@ import android.support.annotation.NonNull;
 import android.support.annotation.StyleRes;
 import android.util.Log;
 
+import net.oldev.alsscratchpad.LSScratchPadModel.ThemeOption;
+
 import java.util.Calendar;
 
 // Utility to set the theme for an Activity
@@ -17,7 +19,7 @@ public class ThemeSwitcher {
      * Set the activity to the theme based on the named model
      */
     public static void setTheme(LSScratchPadModel model, Activity activity) {
-        @StyleRes int themeId = findThemeIdByOption(model);
+        @StyleRes int themeId = findThemeIdByOption(model, activity);
         activity.setTheme(themeId);
     }
 
@@ -31,30 +33,37 @@ public class ThemeSwitcher {
         activity.startActivity(refresh);
     }
 
-    private static @StyleRes int getThemeIdByTime(@NonNull LSScratchPadModel model) {
+    private static boolean isDarkEffectiveForAutoTheme(@NonNull LSScratchPadModel model) {
         final TimeRange darkThemeTimeRange = model.getAutoThemeDarkTimeRange();
 
         Calendar curTime = Calendar.getInstance();
-        if (darkThemeTimeRange.contains(curTime)) {
-            return R.style.AppTheme_Dark;
-        } else {
-            return R.style.AppTheme;
-        }
+        return darkThemeTimeRange.contains(curTime);
     }
 
-    private static @StyleRes int findThemeIdByOption(@NonNull LSScratchPadModel model) {
-        @LSScratchPadModel.ThemeOption String theme = model.getTheme();
+    private static @StyleRes int findThemeIdByOption(@NonNull LSScratchPadModel model,
+                                                     @NonNull Activity activity) {
+        @ThemeOption String theme = model.getTheme();
 
+        // For auto case, resolve the actual theme option (light or dark)
+        if (LSScratchPadModel.THEME_AUTO.equals(theme)) {
+            theme = ( isDarkEffectiveForAutoTheme(model) ? LSScratchPadModel.THEME_DARK :
+                    LSScratchPadModel.THEME_LIGHT );
+        }
+
+        // For light or dark option
+        // resolve the actual style based on the option and the activity currently requesting it
+        // (SettingsActivity, based on Android Studio-generated template,
+        //  does not define its own support action bar. Hence the style to be used is of the
+        //  variety that uses system's own action bar, rather than the default _NoActionBar variant)
         @StyleRes int themeId;
         switch (theme) {
-            case LSScratchPadModel.THEME_AUTO:
-                themeId = getThemeIdByTime(model);
-                break;
             case LSScratchPadModel.THEME_DARK:
-                themeId = R.style.AppTheme_Dark;
+                themeId = ( activity instanceof SettingsActivity ? R.style.AppTheme_Dark :
+                        R.style.AppTheme_Dark_NoActionBar );
                 break;
             case LSScratchPadModel.THEME_LIGHT:
-                themeId = R.style.AppTheme;
+                themeId = ( activity instanceof SettingsActivity ? R.style.AppTheme :
+                        R.style.AppTheme_NoActionBar );
                 break;
             default:
                 Log.w(TAG, "Unexpected theme option +[" + theme + "]. Use default");
