76a166440c68897e602b523592583cbd05851292
orionlee <orionlee@yahoo.com>
2018-03-28 10:34:25 -0700

    Lock Screen Notification tweak: once swiped off, it won't show again until the user has unlocked and relocked the device.

--- app/src/main/java/net/oldev/alsscratchpad/LockScreenReceiver.java
+++ app/src/main/java/net/oldev/alsscratchpad/LockScreenReceiver.java
@@ -22,6 +22,7 @@ public abstract class LockScreenReceiver extends BroadcastReceiver {
     // last relevant intent handled (to dispatch onShowingLockScreen() correctly)
     private Intent mLastIntent;
     private long mLastIntentTimestamp;
+    private boolean mScreenLocked = false;
 
     @Override
     public void onReceive(Context context, Intent intent) {
@@ -44,12 +45,36 @@ public abstract class LockScreenReceiver extends BroadcastReceiver {
             {
                 // Screen is locked, (technically, becomes non-interactive, screen might still be on)
                 Log.d(TAG, "[SCREEN_OFF]");
-                onLocked();
+                if (!mScreenLocked) {
+                    mScreenLocked = true;
+                    onLocked();
+                } else {
+                    // Avoid repeatedly calling onLocked on lock screen. Scenario:
+                    // - User locks the device:
+                    //   - SCREEN_OFF received. onLock() is invoked.
+                    // - User turns on and sees the lock screen:
+                    //   - SCREEN_ON received. onShowingLockScreen() is invoked.
+                    // - User turns off the screen (without unlocking the phone)
+                    //   - SCREEN_OFF received. The state tracking mScreenLocked prevents onLocked() from being invoked.
+                    // - Use finally unlocks the device
+                    //   - USER_PRESENT received. onUnlocked() is invoked.
+                    //
+                    // Preventing onLocked() being invoked repeatedly in locked state is important
+                    // for lock screen notification use case. The logic supports the behavior
+                    // - On lock screen, user decides he/she does not need LS Scratch Pad and swipes off the notification.
+                    // - The user then turns off the screen (without unlocking)
+                    // - The user turns on the screen (lock screen). He/she would not want to see the lockscreen notification
+                    //
+                    // The logic here prevents onLocked() being invoked again, thus preventing lockscreen notification being
+                    // shown again.
+                    Log.d(TAG, "  SCREEN_OFF ignored: the screen has already been locked. The user just turns off the screen after seeing the lock screen (without any unlock).");
+                }
             }
             else if (intent.getAction().equals(Intent.ACTION_USER_PRESENT))
             {
                 // Screen is unlocked
                 Log.d(TAG, "[USER_PRESENT]");
+                mScreenLocked = false;
                 onUnlocked();
             } else {
                 return;
