803882d0247d9aede42d76e1e59213062d5995aa
orionlee <orionlee@yahoo.com>
2018-04-26 13:29:39 -0700

    (Halted) UI trial in showing a message upon (1st) pressing QSTile on unlocked screen. Unsatisfactory results of two approaches: 1) use Toast with a custom layout to mimic snackbar UI, 2) (SnackbarWrapper) Access WindowManager to create a snackbar
    - Conceivably, one can combine the above approaches such that
    a) Use a customized layout to accomodate the long message (from custom layout in approach 1)
    b) Use a customize snackbar (or snackbar like construct) so that the message can stay indefinite (from approach 2, with modification)
    There is still the issue that by using variation of approach 2, the app needs to ask user for runtime draw-over permission for Oreo+ devices, which seems to be an overkill for this feature.

--- app/src/main/java/net/oldev/aljotit/LjotItTileService.java
+++ app/src/main/java/net/oldev/aljotit/LjotItTileService.java
@@ -4,6 +4,16 @@ import android.annotation.TargetApi;
 import android.content.Intent;
 import android.os.Build;
 import android.service.quicksettings.TileService;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.design.widget.Snackbar;
+import android.text.TextUtils;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 @TargetApi(Build.VERSION_CODES.N)
 public class LjotItTileService extends TileService {
@@ -25,6 +35,7 @@ public class LjotItTileService extends TileService {
                                 model.getSendToPreferredClassName());
             intent.putExtra(Intent.EXTRA_TEXT, "");
             intent.setType("text/plain");
+            showUnlockedScreenHelpMessageIfNotShown();
         }
         startActivityAndCollapse(intent);
     }
@@ -44,6 +55,48 @@ public class LjotItTileService extends TileService {
 
     private LjotItModel getModel() {
         return LjotItApp.getApp(this).getModel();
+    }
+
+
+    private void showUnlockedScreenHelpMessageIfNotShown() {
+        final String msg = "On unlocked screen, press the quick settings tile to create a new Keep note.\nLong-press to bring up LjotIt.";
+        //showSnackBarLikeToast(msg, null, null);
+        //showMessage(msg);
+    }
+
+    private void showMessage(@NonNull String msg) {
+        SnackbarWrapper snackbarWrapper = SnackbarWrapper.make(getApplicationContext(),
+                msg, Snackbar.LENGTH_LONG);
+        snackbarWrapper.show();
+    }
+
+    private void showSnackBarLikeToast(@NonNull String msg,
+                                       @Nullable String actionText,
+                                       @Nullable View.OnClickListener actionOnClickListener) {
+
+        //inflate the custom toast
+        LayoutInflater inflater = LayoutInflater.from(getApplicationContext());
+        View layout = inflater.inflate(R.layout.snackbar_like_toast, null);
+
+        // Set the Text to show in TextView
+        TextView text = (TextView) layout.findViewById(R.id.snackbar_text);
+        text.setText(msg);
+
+        Button button = (Button) layout.findViewById(R.id.snackbar_action);
+        if (!TextUtils.isEmpty(actionText) && actionOnClickListener != null) {
+            button.setText(actionText);
+            button.setOnClickListener(actionOnClickListener);
+        } else {
+            button.setVisibility(View.GONE);
+        }
+        Toast toast = new Toast(getApplicationContext());
+
+        //Setting up toast position, similar to Snackbar
+        // Set Gravity.TOP (rather than the customary BOTTOM) to avoid colliding with soft keyboard
+        toast.setGravity(Gravity.TOP | Gravity.LEFT  | Gravity.START | Gravity.FILL_HORIZONTAL, 0, 0);
+        toast.setDuration(Toast.LENGTH_LONG);
+        toast.setView(layout);
+        toast.show();
     }
 
 }
